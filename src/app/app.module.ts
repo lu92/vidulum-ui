ccba2f810fdc3b209c99e319168daec995cee126
lu92 <biklucjan@gmail.com>
2021-07-15 20:20:57 +0200

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,28 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button'
 import { MatSliderModule } from '@angular/material/slider';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
       BrowserModule,
       BrowserAnimationsModule,
       MatButtonModule,
-      MatSliderModule
+      MatSliderModule,
+      LayoutModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
